e613c57d2dd97b6f37309d2a041e8d80281b772f
Manish <manishchaudhary220702@gmail.com>
2024-05-27 11:54:58 +0530

    CHange code for csv

--- routes/csvRoute.js
+++ routes/csvRoute.js
@@ -1,3 +1,47 @@
+// const express = require("express");
+// const multer = require('multer');
+// const path = require('path');
+// const router = express.Router();
+// const { adminAuth } = require("../middleware/auth");
+
+// const databaseController = require("../controllers/databaseController");
+
+// // Determine upload directory based on environment
+// const uploadDir = process.env.NODE_ENV === 'production' ? '/tmp' : path.join(__dirname, '../uploads');
+
+// // Ensure the upload directory exists
+// const fs = require('fs');
+// if (!fs.existsSync(uploadDir)) {
+//     fs.mkdirSync(uploadDir, { recursive: true });
+// }
+
+// // Multer setup for file uploads
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, uploadDir);
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, file.originalname);
+//     }
+// });
+
+// const upload = multer({ storage });
+
+// // CSV upload route
+// router.post('/importUser', adminAuth, upload.single('file'), databaseController.importUser);
+
+// // Route to delete all lead data
+// router.delete('/deleteAllLeadData', adminAuth, databaseController.deleteAllLeadData);
+
+// module.exports = router;
+
+
+
+
+
+
+
+
 const express = require("express");
 const multer = require('multer');
 const path = require('path');
@@ -6,19 +50,10 @@ const { adminAuth } = require("../middleware/auth");
 
 const databaseController = require("../controllers/databaseController");
 
-// Determine upload directory based on environment
-const uploadDir = process.env.NODE_ENV === 'production' ? '/tmp' : path.join(__dirname, '../uploads');
-
-// Ensure the upload directory exists
-const fs = require('fs');
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
-
 // Multer setup for file uploads
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, uploadDir);
+        cb(null, '/tmp');  // Use /tmp for temporary storage in Vercel
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
@@ -34,3 +69,4 @@ router.post('/importUser', adminAuth, upload.single('file'), databaseController.
 router.delete('/deleteAllLeadData', adminAuth, databaseController.deleteAllLeadData);
 
 module.exports = router;
+
